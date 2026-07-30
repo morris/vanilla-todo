a5aae8057f8dd1c5225971a3cf16dfa1f4688082
Morris Brodersen <mb@morrisbrodersen.de>
2026-07-30 15:38:37 +0200

    use crypto randomUUID if available

--- public/scripts/uuid.js
+++ public/scripts/uuid.js
@@ -1,4 +1,8 @@
 export function uuid() {
+  if (typeof window.crypto?.randomUUID === 'function') {
+    return crypto.randomUUID();
+  }
+
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
     const r = (Math.random() * 16) | 0,
       v = c === 'x' ? r : (r & 0x3) | 0x8;
